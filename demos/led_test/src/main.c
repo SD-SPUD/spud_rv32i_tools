3d8fc76905940666114bc966ace3aaa0b66dad84
Frank Laterza <franklaterza@gmail.com>
2025-10-20 10:29:10 -0400

    hi val pt2

--- demos/led_test/src/main.c
+++ demos/led_test/src/main.c
@@ -1,15 +1,21 @@
 #include "spudkit.h"
 
+void sleepx(int x){
+    for(int i = 0; i < x; i++) {
+        __asm__ volatile("nop");
+    }
+}
+
 int main() {
     // initialize the spudkit library
     spudkit_init();
     volatile uint32_t* led_ctrl = (volatile uint32_t*)0x95000000;
+    int count = 0;
     while (1) {
-        *led_ctrl = 1;
-        for (int i = 0; i < 1000000; i++) {
-            __asm__("nop");
-        }
         *led_ctrl = 0;
+        sleepx(1000000);
+        *led_ctrl = 1;
+        sleepx(1000000);
     }
     return 0;
 }
